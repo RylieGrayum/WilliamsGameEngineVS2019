6800a209d4dcb3c56eae414a7654ced1a172e09b
Rylie Grayum <grayums@rsd407.org>
2021-03-24 01:08:57 -0700

    Added 3 lasers and moving meteors. Also added collisions but they arn't working yet.

---
+++ Base project/MyGame/MyGame/Meteor.cpp
@@ -0,0 +1,44 @@
+#include "Meteor.h"
+
+const float SPEED = 0.25f;
+
+Meteor::Meteor(sf::Vector2f pos)
+{
+	sprite_.setTexture(GAME.getTexture("Resources/meteor.png"));
+	sprite_.setPosition(pos);
+	assignTag("meteor");
+}
+
+void Meteor::draw()
+{
+	GAME.getRenderWindow().draw(sprite_);
+}
+
+void Meteor::update(sf::Time& elapsed) {
+	int msElapsed = elapsed.asMilliseconds();
+	sf::Vector2f pos = sprite_.getPosition();
+
+	if (pos.x < sprite_.getGlobalBounds().width * -1)
+	{
+		makeDead();
+	}
+	else
+	{
+		sprite_.setPosition(sf::Vector2f(pos.x - SPEED * msElapsed, pos.y));
+	}
+}
+
+void Meteor::handleCollision(GameObject& otherGameObject)
+{
+	if (otherGameObject.hasTag("laser"))
+	{
+		otherGameObject.makeDead();
+	}
+
+	makeDead();
+}
+
+sf::FloatRect Meteor::getCollisionRect()
+{
+	return sprite_.getGlobalBounds();
+}
--- Base project/MyGame/MyGame/Ship.cpp
+++ Base project/MyGame/MyGame/Ship.cpp
@@ -1,6 +1,8 @@
 #include "Ship.h"
+#include "Laser.h"
 
 const float SPEED = 0.3f;
+const int FIRE_DELAY = 200;
 
 Ship::Ship()
 {
@@ -26,6 +28,40 @@ void Ship::update(sf::Time& elapsed) {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))   x += SPEED * msElapsed;
 
 	sprite_.setPosition(sf::Vector2f(x, y));
+
+	if (fireTimer_ > 0)
+	{
+		fireTimer_ -= msElapsed;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && fireTimer_ <= 0)
+	{
+		fireTimer_ = FIRE_DELAY;
+
+		sf::FloatRect bounds = sprite_.getGlobalBounds();
+
+		// Top Laser
+		float laserXtop = x + (bounds.width / 2);
+		float laserYtop = y + 0;
+
+		// Middle Laser
+		float laserXmiddle = x + bounds.width;
+		float laserYmiddle = y + (bounds.height / 2.0f);
+
+		// Bottom Laser
+		float laserXbottom = x + (bounds.width / 2);
+		float laserYbottom = y + bounds.height;
+
+		LaserPtr topLaser = std::make_shared<Laser>(sf::Vector2f(laserXtop, laserYtop));
+		GAME.getCurrentScene().addGameObject(topLaser);
+
+		LaserPtr middleLaser = std::make_shared<Laser>(sf::Vector2f(laserXmiddle, laserYmiddle));
+		GAME.getCurrentScene().addGameObject(middleLaser);
+
+		LaserPtr bottomLaser = std::make_shared<Laser>(sf::Vector2f(laserXbottom, laserYbottom));
+		GAME.getCurrentScene().addGameObject(bottomLaser);
+
+	}
 
 
 
